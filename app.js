86c112526c492f4235502aa6f4fd5462275866f8
abhishek sindhwal <abhisheksindhwal11@gmail.com>
2026-02-01 18:09:10 +0530

    Initial commit - real time location tracker

---
+++ app.js
@@ -0,0 +1,35 @@
+import  express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
+import  path  from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const app=express();
+
+const server=http.createServer(app)
+const io= new Server(server)
+
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+
+app.set("view engine", "ejs");
+// app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection",function(socket){
+     socket.on("send-location",function(data){
+          io.emit("receive-location",{id:socket.id,...data});
+     })
+     socket.on("disconnect",function(){
+          io.emit("user-disconnected",socket.id);
+     })
+})
+app.get("/", function(req,res) {
+     res.render("index")
+});
+
+
+server.listen(3000, "0.0.0.0", () => {
+     console.log("Server running");
+   });
